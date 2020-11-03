19a6b9aa9537584fa0fdec8841db7c26877d9cf5
674745853@qq.com <199606018>
2020-11-03 11:13:29 +0800

    # 新增数据源调用地方

--- src/main/java/org/apache/ibatis/executor/statement/PreparedStatementHandler.java
+++ src/main/java/org/apache/ibatis/executor/statement/PreparedStatementHandler.java
@@ -61,6 +61,7 @@ public class PreparedStatementHandler extends BaseStatementHandler {
   @Override
   public <E> List<E> query(Statement statement, ResultHandler resultHandler) throws SQLException {
     PreparedStatement ps = (PreparedStatement) statement;
+    // 调用数据源
     ps.execute();
     return resultSetHandler.handleResultSets(ps);
   }
